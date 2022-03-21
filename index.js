97c58dac3ad59a674f3c43544e40c8acf856ad70
janinquisitor <jancarlos0195@gmail.com>
2022-03-21 16:59:31 -0400

    Added classes

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const writeToFile = require('./src/fileControllers')
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+
+let employee = new Employee('Random Dude', 1, 'example@gmail.com');
+let manager = new Manager('Jan Carlos', 999, 'boss@example.com', '555-5555');
+
+console.log(employee)
+console.log(manager)
+
 
